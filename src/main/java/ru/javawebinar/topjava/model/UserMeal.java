7a985e880db74a0a478fef6a822e11f6893239dd
glukoman <glukoman86@gmail.com>
2020-08-29 16:53:59 +0300

    Implement filter methods

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class UserMeal {
     private final LocalDateTime dateTime;
@@ -25,5 +27,33 @@ public class UserMeal {
 
     public int getCalories() {
         return calories;
+    }
+
+    public LocalDate getDate() {
+        return getDateTime().toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return getDateTime().toLocalTime();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserMeal userMeal = (UserMeal) o;
+
+        if (calories != userMeal.calories) return false;
+        if (!dateTime.equals(userMeal.dateTime)) return false;
+        return description.equals(userMeal.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = dateTime.hashCode();
+        result = 31 * result + description.hashCode();
+        result = 31 * result + calories;
+        return result;
     }
 }
