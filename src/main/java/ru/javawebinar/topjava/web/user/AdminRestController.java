0b5a4796d9680ef7d943e6f7d07d2696fd1f3139
glukoman <glukoman86@gmail.com>
2020-12-04 15:17:11 +0300

    9_03_HW8_enable_disable

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,5 +65,12 @@ public class AdminRestController extends AbstractUserController {
     @GetMapping("/by")
     public User getByMail(@RequestParam String email) {
         return super.getByMail(email);
+    }
+
+    @Override
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable int id, @RequestParam boolean enabled) {
+        super.enable(id, enabled);
     }
 }
